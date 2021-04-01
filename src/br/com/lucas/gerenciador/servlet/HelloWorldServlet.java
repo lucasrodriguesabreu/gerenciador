8a802d90c551358a047396b6eb6d67ff003394e4
Lucas Rodrigues Abreu <lucasrodriguesabreu@hotmail.com>
2021-04-01 11:00:30 -0300

    Subindo meu primeiro Servlet

---
+++ src/br/com/lucas/gerenciador/servlet/HelloWorldServlet.java
@@ -0,0 +1,29 @@
+package br.com.lucas.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/oi")
+public class HelloWorldServlet extends HttpServlet{
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Hello World! Você criou o primeiro servlet!");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("O Servlet HelloWorldServlet foi chamado com sucesso!");
+		
+	}
+}
