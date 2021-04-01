429b5229619171d9cd3a5201635825de6b19950b
Lucas Rodrigues Abreu <lucasrodriguesabreu@hotmail.com>
2021-04-01 13:37:42 -0300

    Implementado método doPost ao invés de service

--- src/br/com/lucas/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/br/com/lucas/gerenciador/servlet/NovaEmpresaServlet.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		System.out.println("Cadastrando nova empresa");
 		
@@ -26,9 +26,6 @@ public class NovaEmpresaServlet extends HttpServlet {
 		out.println("Empresa " + nomeEmpresa + " cadastrada com sucesso!");
 		out.println("</body>");
 		out.println("</html>");
-		
-		
-		
+				
 	}
-
 }
