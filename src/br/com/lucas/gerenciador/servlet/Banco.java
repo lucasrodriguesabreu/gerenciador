808bf60b51ec64f8fc01828c6d094d1804c675b5
Lucas Rodrigues Abreu <lucasrodriguesabreu@hotmail.com>
2021-04-01 14:57:34 -0300

    Implementando listagem de empresas

--- src/br/com/lucas/gerenciador/servlet/Banco.java
+++ src/br/com/lucas/gerenciador/servlet/Banco.java
@@ -7,6 +7,16 @@ public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
 	
+	/*Simula os dados salvos no banco de dados
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setNome("Google");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Facebook");
+		lista.add(empresa);
+		lista.add(empresa2);
+	}*/
+	
 	public void adiciona(Empresa empresa) {
 		Banco.lista.add(empresa);
 		System.out.println("Empresa " + empresa + " adicionada com sucesso!");
