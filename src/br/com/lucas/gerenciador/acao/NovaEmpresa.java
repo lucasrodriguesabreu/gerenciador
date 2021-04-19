b110ae7276b0c3f8490cc0704d146a1dcad12447
Lucas Rodrigues Abreu <lucasrodriguesabreu@hotmail.com>
2021-04-19 18:10:19 -0300

    Centralizando o redirecionamento, adicionar e editar implementado

--- src/br/com/lucas/gerenciador/acao/NovaEmpresa.java
+++ src/br/com/lucas/gerenciador/acao/NovaEmpresa.java
@@ -14,7 +14,7 @@ import br.com.lucas.gerenciador.modelo.Empresa;
 
 public class NovaEmpresa {
 	
-	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		
 		System.out.println("Cadastrando nova empresa");
 		
@@ -39,7 +39,7 @@ public class NovaEmpresa {
 		
 		request.setAttribute("empresa", empresa.getNome());
 		
-		response.sendRedirect("entrada?acao=ListaEmpresas");
+		return "redirect:entrada?acao=ListaEmpresas";
 
 	}
 }
