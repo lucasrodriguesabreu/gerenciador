ae3edfb7c552902cf01178ee0778eb9d007ae278
Lucas Rodrigues Abreu <lucasrodriguesabreu@hotmail.com>
2021-04-22 10:27:20 -0300

    Implementado filtro para monitoramento de tempo de execução

--- src/br/com/lucas/gerenciador/acao/ListaEmpresas.java
+++ src/br/com/lucas/gerenciador/acao/ListaEmpresas.java
@@ -14,19 +14,14 @@ import br.com.lucas.gerenciador.modelo.Empresa;
 public class ListaEmpresas implements Acao{
 	
 	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		HttpSession sessao = request.getSession();
-		if(sessao.getAttribute("usuarioLogado") == null) {
-			return "redirect:entrada?acao=LoginForm";
-		}
-		
+					
 		System.out.println("Listando empresas");
 		
 		Banco banco = new Banco();
     	List<Empresa> lista = banco.getEmpresas();
     	
     	request.setAttribute("empresas", lista);
-    	
+    	    	   	
     	return "forward:listaEmpresas.jsp";
 
 	}
