5ae9e3b57f0ceb292c3b86a8851f58565294dd44
Lucas Rodrigues Abreu <lucasrodriguesabreu@hotmail.com>
2021-04-23 09:18:54 -0300

    Implementando webservice com json e xml

---
+++ src/br/com/lucas/gerenciador/servlet/ListaEmpresasService.java
@@ -0,0 +1,43 @@
+package br.com.lucas.gerenciador.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.lucas.gerenciador.modelo.Banco;
+import br.com.lucas.gerenciador.modelo.Empresa;
+
+@WebServlet("/empresas")
+public class ListaEmpresasService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		List<Empresa> empresas = new Banco().getEmpresas();
+		
+//Usando XML		
+		
+		XStream xstream = new XStream();
+		xstream.alias("empresa", Empresa.class);
+		String xml = xstream.toXML(empresas);
+		
+		response.setContentType("application/xml");
+		response.getWriter().print(xml);
+		
+// Usando json		
+//		Gson gson = new Gson();
+//		String json = gson.toJson(empresas);
+//		
+//		response.setContentType("application/json");
+//		response.getWriter().print(json);
+		
+	}
+
+}
